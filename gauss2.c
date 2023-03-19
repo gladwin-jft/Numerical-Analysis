6bab17398b6acfd3322f8c9b3869feea16a963b7
gladwin-jft <128125104+gladwin-jft@users.noreply.github.com>
2023-03-19 19:47:59 +0530

    Update gauss2.c
    i just put lala l al a

--- gauss2.c
+++ gauss2.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<math.h>
-//float mat[4][4]= {{2, 1, -1, 8}, {-3, -1, 2, -11}, {-2, 1, 2, -3}};
+//float mat[4][4]= {{2, 1, -1, 8}, {-3, -1, 2, -11}, {-2, 1, 2, -3}};la la al al la al al al ala 
 float mat[4][4] = {{1, 1, 2, 8}, {-1, -2, 3, 1}, {3, -7, 4, 10}};
 int i,n = 3,j;
 float pivot;
